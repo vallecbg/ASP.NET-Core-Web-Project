58bcc34f99be9780be7a2d4fe2fa643613bf70ed
vallecbg <valioplayofficial@gmail.com>
2019-07-24 18:31:03 +0300

    Added CommentsController Tests

---
+++ BookCreator/BookCreator.Tests/Controllers/Comments/CommentsControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Microsoft.AspNetCore.Authorization;
+using NUnit.Framework;
+
+namespace BookCreator.Tests.Controllers.Comments
+{
+    [TestFixture]
+    public class CommentsControllerTests
+    {
+        [Test]
+        public void Controller_Should_Contains_Authorize()
+        {
+            typeof(BookCreatorApp.Controllers.CommentsController)
+                .Should()
+                .BeDecoratedWith<AuthorizeAttribute>();
+        }
+    }
+}
